53680bc27a95a925d677ca9f68a51ca2d73a2bdc
paronson <paronson@qw-corp.com>
2023-12-24 09:05:49 -0600

    fix fetch annotation header

--- client/src/store/annotation.js
+++ client/src/store/annotation.js
@@ -1,9 +1,8 @@
 import { defineStore } from "pinia";
 import config from "./../../config/config";
 
-
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export const useAnnotationStore = defineStore("annotation", {
@@ -14,80 +13,46 @@ export const useAnnotationStore = defineStore("annotation", {
         };
     },
     actions: {
-        async fetchAllAnnotations(username = null, accessToken = null) {
+        async fetchAllAnnotations() {
             const { annotApiEndpoint, rateLimitMs } = config.connections.annotation;
-            const userFilter = import.meta.env.VITE_ANNOT_USER_FILTER;
-            const apiKey = import.meta.env.VITE_ANNOT_API_KEY;
+            const userFilter = import.meta.env.VITE_ANNOT_USER_FILTER || "";
+            const apiKey = import.meta.env.VITE_ANNOT_API_KEY || "";
 
             let searchAfter = null;
 
             const headers = {
                 Authorization: `Bearer ${apiKey}`,
-                "Content-Type": "application/json",
+                Accept: "application/vnd.hypothesis.v1+json",
             };
 
-            while (true) {
-                let url = `${annotApiEndpoint}?user=${userFilter}&sort=updated`;
-                if (searchAfter) {
-                    url += "&search_after=" + encodeURIComponent(searchAfter);
+            if (!userFilter || !apiKey) {
+                console.log("Annotations is not configured..");
+            } else {
+                while (true) {
+                    let url = `${annotApiEndpoint}?user=${userFilter}&sort=updated`;
+                    if (searchAfter) {
+                        url += "&search_after=" + encodeURIComponent(searchAfter);
+                    }
+
+                    const response = await fetch(url, { method: "GET", headers });
+                    if (!response.ok) {
+                        throw new Error(`Error fetching annotations: ${response.statusText}`);
+                    }
+
+                    const data = await response.json();
+                    if (data.rows && data.rows.length > 0) {
+                        this.annotations = this.annotations.concat(data.rows);
+                        searchAfter = data.rows[data.rows.length - 1].updated;
+                    } else {
+                        break;
+                    }
+
+                    // Rate limit
+                    await sleep(rateLimitMs);
                 }
 
-                const response = await fetch(url, { method: "GET", headers });
-                if (!response.ok) {
-                    throw new Error(`Error fetching annotations: ${response.statusText}`);
-                }
-
-                const data = await response.json();
-                if (data.rows && data.rows.length > 0) {
-                    this.annotations = this.annotations.concat(data.rows);
-                    searchAfter = data.rows[data.rows.length - 1].updated; // Assuming 'updated' field is used for pagination
-                } else {
-                    break;
-                }
-
-                // Rate limit - wait for specified milliseconds
-                await sleep(rateLimitMs);
+                return this.annotations;
             }
-
-            return this.annotations;
         },
-
-        // async fetchAllAnnotations() {
-        //     const API_ENDPOINT = config.connections.annotation.annotApiEndpoint;
-        //     const USER_FILTER = import.meta.env.VITE_ANNOT_USER_FILTER;
-        //     const apiKey = import.meta.env.VITE_ANNOT_API_KEY;
-        //     let offset = 0;
-        //     let hasMore = true;
-
-        //     while (hasMore) {
-        //         try {
-        //             const response = await fetch(`${API_ENDPOINT}?user=${USER_FILTER}&offset=${offset}`, {
-        //                 method: "GET",
-        //                 headers: {
-        //                     Authorization: `Bearer ${apiKey}`,
-        //                 },
-        //             });
-
-        //             if (!response.ok) {
-        //                 throw new Error(`HTTP error! status: ${response.status}`);
-        //             }
-
-        //             const data = await response.json();
-        //             this.annotations = this.annotations.concat(data.rows);
-
-        //             // Check if there are more results to fetch
-        //             hasMore = data.rows.length > 0;
-        //             offset += data.rows.length;
-
-        //             // Add delay to respect rate limits
-        //             await new Promise((resolve) => setTimeout(resolve, 100));
-        //         } catch (error) {
-        //             console.error("Error fetching annotations:", error);
-        //             break;
-        //         }
-        //     }
-
-        //     return this.annotations;
-        // },
     },
 });
