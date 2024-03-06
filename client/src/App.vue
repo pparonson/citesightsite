5714d908de2bb019c38f54a79b068ad12be5ca9c
paronson <paronson@qw-corp.com>
2024-03-06 09:55:19 -0600

    revert App.vue and inc version.json

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,11 @@
 <template>
     <div id="app">
-        <!-- <RouterView :key="routeKey" /> -->
         <RouterView />
     </div>
 </template>
 
-<script>
-    import { computed, ref, watch } from 'vue';
-    import { RouterView, useRoute } from "vue-router";
-export default {
-  setup() {
-    // const route = useRoute();
-    // const uniqueKey = ref(Date.now()); // set an initial value
-    //
-    // watch(() => route.path, (newPath, oldPath) => {
-    //   if (newPath === '/' && newPath !== oldPath) {
-    //     uniqueKey.value = Date.now(); // any unique value to trigger component recreation
-    //   }
-    // });
-    //
-    // const routeKey = computed(() => `${route.path}-${uniqueKey.value}`);
-
-    // return { routeKey };
-    return {};
-  },
-};
+<script setup>
+    import { RouterView } from "vue-router";
 </script>
 
 <style>
