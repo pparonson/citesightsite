c78c30378529888edaeec98ad5a6874db8507dc5
paronson <paronson@qw-corp.com>
2024-04-14 17:26:31 -0500

    refactor and cleanup

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    defaultTimeout: 35000,
-    connections: {
-        dockerUserDefinedNetwork: {
-            server: { url: "http://server", port: 4000 },
-            worker: { url: "http://worker", port: 5000 },
-        },
-    },
-};
