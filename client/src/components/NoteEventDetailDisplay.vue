d69d072f0700b9dd5c6d305243f49d01abab1466
paronson <paronson@qw-corp.com>
2024-10-05 19:56:07 -0500

    add additional events display support

--- client/src/components/NoteEventDetailDisplay.vue
+++ client/src/components/NoteEventDetailDisplay.vue
@@ -1,9 +1,43 @@
 <template>
     <div class="h-full p-2 my-1 bg-gray-100 rounded-md overflow-y-auto" @dblclick="handleDoubleClick">
-        <Tags :tags="noteEvent?.tags || []" />
-        <div>{{ noteTitle }}</div>
-        <p class="text-base text-xs overflow-hidden">ID: {{ noteEvent?.id }}</p>
-        <div class="" v-html="renderedContent"></div>
+        <Tags :tags="displayedTags" />
+        <div class="text-xl font-bold mb-2">{{ displayedTitle }}</div>
+        <!-- <p class="text-base text-xs overflow-hidden">ID: {{ noteEvent?.id }}</p> -->
+        <!-- <p class="text-xs mb-2">Type: {{ noteEvent?.type }}</p> -->
+        
+        <p v-if="isAnnotation" class="text-sm mb-2">
+            <strong>URI:  </strong> 
+            <a :href="noteEvent.uri" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                class="text-blue-500 hover:text-blue-700 underline"
+                >{{ noteEvent.uri }}</a>
+        </p>
+        <div v-if="isAnnotation && noteEvent.links && noteEvent.links.length > 0" class="mb-2">
+            <strong>Links:  </strong>
+            <ul>
+                <li v-for="link in noteEvent.links" :key="link.href">
+                    <a :href="link.href" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        >{{ link.href }}</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="mb-4" v-html="renderedContent"></div>
+       
+        <div class="mb-2">
+            <p class="text-base text-xs overflow-hidden"><strong>ID:  </strong> {{ noteEvent?.id }}</p>
+            <p class="text-xs"><strong>Type:  </strong> {{ noteEvent?.type }}</p>
+            <p class="text-xs">
+                <strong>Created:  </strong> 
+                {{ noteEvent.created ? formatDate(noteEvent.created) : noteEvent.created_at ? formatDate(noteEvent.created_at) : "Invalid date" }}
+            </p>
+            <p v-if="isAnnotation" class="text-xs">
+                <strong>Updated:  </strong> {{ formatDate(noteEvent.updated) }}
+            </p>
+        </div>
     </div>
 </template>
 
@@ -11,7 +45,7 @@
     import MarkdownIt from "markdown-it";
     import DOMPurify from "dompurify";
     import Tags from "@/components/Tags.vue";
-    import { ref, watch } from "vue";
+    import { ref, watch, computed } from "vue";
     import { useNostrStore } from "@/store/nostr";
     import { useRoute, useRouter } from "vue-router";
     import { storeToRefs } from "pinia";
@@ -41,6 +75,26 @@ function updateLinksToOpenInNewTabs(htmlString) {
             const noteTitle = ref("");
             const renderedContent = ref("");
             const noteEvent = ref(null);
+            const formatDate = (dateString) => {
+                return new Date(dateString).toLocaleString();
+            };
+
+            const isAnnotation = computed(() => noteEvent.value?.type === "annotation");
+            const displayedTitle = computed(() => {
+                if (isAnnotation.value) {
+                    return noteEvent.value.document?.title[0] || "Untitled Annotation";
+                } else {
+                    const titleTag = noteEvent.value?.tags?.find(([key]) => key === "title");
+                    return titleTag ? titleTag[1] : "Unknown Title";
+                }
+            });
+            const displayedTags = computed(() => {
+                if (isAnnotation.value) {
+                    return noteEvent.value.tags || [];
+                } else {
+                    return noteEvent.value?.tags || [];
+                }
+            });
 
             const handleDoubleClick = () => {
                 if (noteEvent.value?.id) {
@@ -54,7 +108,8 @@ function updateLinksToOpenInNewTabs(htmlString) {
                 noteTitle.value = titleTag ? titleTag[1] : "Unknown Title";
 
                 // Markdown update
-                const content = noteEvent?.value?.content || "";
+                // const content = noteEvent?.value?.content || "";
+                const content = isAnnotation.value ? noteEvent.value.text : (noteEvent.value?.content || "");
                 let markdownOutput = md.render(content);
                 // Update hyperlinks to open in new tabs
                 markdownOutput = updateLinksToOpenInNewTabs(markdownOutput);
@@ -68,6 +123,10 @@ function updateLinksToOpenInNewTabs(htmlString) {
                 renderedContent,
                 noteEvent,
                 handleDoubleClick,
+                isAnnotation,
+                displayedTags,
+                displayedTags,
+                formatDate
             };
         },
     };
