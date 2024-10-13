df0351d67254d97a4a87b8e29ecd0ea4815e032d
paronson <paronson@qw-corp.com>
2024-10-12 21:43:22 -0500

    update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   server: {
-//     host: '0.0.0.0',
-//     port: 3000
-//   },
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
+import version from "./version.json";
 
 export default defineConfig((mode) => {
-    // Load environment variables
-    // const env = loadEnv(mode, process.cwd());
+    console.log("mode:", mode);
+    console.log("version:", version);
 
     return {
         plugins: [vue()],
